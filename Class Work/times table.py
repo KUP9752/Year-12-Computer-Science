415cac3b130bcd30eb34cdb8502c1f2e8cbe0baa
KUP9752 <54801550+KUP9752@users.noreply.github.com>
2019-09-11 11:39:59 +0100

    Times Table made more user friendly
    Couldnt figure out how to do the type check for integers

--- Class Work/times table.py
+++ Class Work/times table.py
@@ -1,23 +1,38 @@
 #Times Table
-
+print('Times table, please enter a number in the range [1,20] for the table.')
 run = 1
+
+
 while run ==1:
-    table = input('What Table')
-    rows = input("How many rows")
-    try:
-        table = table +1
+    table = int(input('Input Table [1-20]:'))
+    if table>0 and table<21:
+        rows = int(input("How many rows[1-20]"))
+        if rows>1 and rows<21:
+            run = 0
+            for item in range(1, rows + 1):
+                print(item, " x ", table, " = ", item*table)
+            print()
+            repeat = input("Would you like to use the Times Table Again? [Yes/No]")
+            if repeat =="yes" or repeat =="Yes":
+                run = 1
+            elif repeat == "No" or repeat =="no":
+                print("Thanks for using the Times Table")
+                print()
+            #End if
         
-        if int(table)/1 == int(table) and int(rows)/1 ==int(rows):
-            run = 2
-            table = int(table)
-            rows = int(table)
+     
         else:
             run = 1
-            print("Invalid input, Please Enter an Integer")
-        #End If
-    except TypeError:
+            print("Invalid Input, try again")
+        #End if
         
-#EndWhile
+    else:
+        run = 1
+        print("Invalid Input, try again")
+    #End if
+    
 
-for item in range(1, rows + 1):
-    print(item, " x ", table, " = ", item*table)
+        
+
+
+
