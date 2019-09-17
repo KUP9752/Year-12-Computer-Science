b06760c4c8c84026103299b14d0508fa2ca134bc
KUP9752 <54801550+KUP9752@users.noreply.github.com>
2019-09-17 09:43:47 +0100

    Notes added

---
+++ Class Work/trying procedure.py
@@ -0,0 +1,23 @@
+
+def goodbye():
+    print("It has been nice to meet you")
+#end procedure
+
+def hello():
+    print("Hello, how are you today?")
+#end procedure
+
+hello()
+print("I am a computer program")
+goodbye()
+
+
+def proc1(x,y):
+    x = x-2
+    y = 0
+#end procedure
+
+m = 8
+n =10
+proc1(m,n)
+print(m,n)
