0c82cdfd36efad4a4303f1f9adb3b2799f017a7b
KUP9752 <54801550+KUP9752@users.noreply.github.com>
2019-10-08 14:10:42 +0100

    updates

---
+++ Work/PYTHON CODE/Encryption-Decryption/decrypt.py
@@ -0,0 +1,40 @@
+#Cipher-> Decrypt
+
+##f = open("output.txt", "rt")
+##data = f.read()
+##f.close()
+data = "z"
+step = int(input("Enter the step of the encryption to Decrypt:   "))
+
+def cipher(n):
+    for i in range(0, len(data)):
+        if data[i] == " ":
+                index = chr(32)
+                print(index, end='')
+        elif data[i] == "(":
+            index = chr(40)
+            print(index, end='')
+        elif data[i] == ")":
+            index = chr(41)
+            print(index, end='')
+        elif data[i] == ".":
+            index = chr(46)
+            print(index, end='')
+        elif data[i] == ",":
+            index = chr(44)
+            print(index, end='')
+        elif data[i].isupper():
+            index = chr((ord(data[i])-n+65)%26 +65)
+            print(index, end='')
+        elif data[i].islower():
+            index = chr((ord(data[i])-n+97)%26 + 97)
+            print(index, end='')
+        else:
+            index = data[i]
+            print(index, end='')
+        #end if
+#next i
+#end procedure
+
+cipher(step)
+
---
+++ Work/PYTHON CODE/Encryption-Decryption/encrypt.py
@@ -0,0 +1,52 @@
+#Cipher-> Encrypt
+
+
+
+step = int(input("Enter the step shift you want:   "))
+
+f = open("input.txt","rt")
+sent = str(f.read())
+f.close()
+
+def cipher(n):
+    for i in range(0,len(sent)):
+        
+            if sent[i] == " ":
+                index = chr(32)
+                print(index, end='')
+            elif sent[i] == "(":
+                index = chr(40)
+                print(index, end='')
+            elif sent[i] == ")":
+                index = chr(41)
+                print(index, end='')
+            elif sent[i] == ".":
+                index = chr(46)
+                print(index, end='')
+            elif sent[i] == ",":
+                index = chr(44)
+                print(index, end='')
+            elif sent[i].isupper():
+                index = chr((ord(sent[i])+ n-65)%26 +65)
+                print(index, end='')
+            elif sent[i].islower():
+                index = chr((ord(sent[i])+n-97)%26 + 97)
+                print(index, end='')
+            
+            else:
+                index = sent[i]
+                print(index, end='')
+            
+        #end if
+    #next i
+#end procedure
+
+cipher(step)
+##p = open("output.txt", "wt")
+##p.write(print(cipher(step)))
+##p.close
+
+
+
+
+
