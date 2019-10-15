04e2450a33d3c1bf6243bb1e1709719f950fa419
KUP9752 <54801550+KUP9752@users.noreply.github.com>
2019-10-15 14:01:44 +0100

    added a fully finctioning CPU and difficulty setting

--- PyGame/pong bouncing ball.py
+++ PyGame/pong bouncing ball.py
@@ -42,7 +42,8 @@ speed = 5
 direction_x = 0
 direction_y = 0
 
-cpu_speed = 5
+cpu_speed = 0
+
 
 
 cpu_block_x = 640-(4+8)
@@ -58,7 +59,7 @@ ball_speed = 2
 
 hp_left= 3
 
-score_1=0
+score_1=0   #//score of PLAYER 1
 ### -- Game Loop
 while not game_over:
     # -- User input and controls
@@ -84,62 +85,78 @@ while not game_over:
 
             
     # -- Game logic goes after this comment
-    dif_counter = score_1
-    if dif_counter == 10:
-        ball_speed = ball_speed +2
-        dif_counter = 0
-
-    #//PLAYER 1 paddle movement
-    if block_y <0:
-        block_y = 0
-    elif block_y > 440:
-        block_y = 440
-    else:
-        block_y = block_y + direction_y * speed
-
-    #//Cpu paddle movement
-    if cpu_block_y > circ_y:
-        cpu_block_y = cpu_block_y + cpu_speed *-1
-    elif cpu_block_y< circ_y:
-        cpu_block_y = cpu_block_y + cpu_speed * 1
-    #end if 
-        
+ 
+    if cpu_speed!=0:
+     #//Cpu paddle movement and calculations (+20 for centering the paddle) 
+        if cpu_block_y+20 > circ_y:
+            cpu_block_y = cpu_block_y + cpu_speed *-1
+        elif cpu_block_y+20 < circ_y:
+            cpu_block_y = cpu_block_y + cpu_speed * 1
+        #end if 
             
-    if circ_x == 12+8 and (circ_y<=block_y+40 and circ_y>=block_y):
-        Cdir_x = Cdir_x * -1
-        circ_x = circ_x + (Cdir_x*ball_speed)
-        score_1 +=1
-        
-    elif circ_x == 0 + 8 or circ_x==640 - 8: 
-        circ_x = 320            #//variables reset so that game restarts
-        circ_y = 240
-        direction_x = 0
-        direction_y = 0
-        Cdir_x = 4
-        Cdir_y = 4
-        
-        hp_left = hp_left -1
-        
-        if hp_left == 0:
-            game_over = True    #//ends game after losing 3 times
-            screen.blit(text, textRect)
-            pygame.display.update() 
-        #end if  
-    else:
-        if circ_x != 0+8 and circ_x != 640-8:
-            circ_x = circ_x + Cdir_x
-        else:
+        if circ_x == cpu_block_x-8 and (circ_y<=cpu_block_y+40 and circ_y>=cpu_block_y):
             Cdir_x = Cdir_x * -1
-            circ_x = circ_x + (Cdir_x*2)
+            circ_x = circ_x + (Cdir_x*ball_speed)
+            
+        if circ_x ==640-8:
+            score_1 +=1
         #end if
-        if circ_y != 0 + 8 and circ_y != 480-8:
-            circ_y = circ_y + Cdir_y
-        else:
-            Cdir_y = Cdir_y * -1
-            circ_y = circ_y+ (Cdir_y*2)
-        #end if
-    #end if
 
+
+            
+        #//PLAYER 1 paddle movement and calculations
+        if block_y <0:
+            block_y = 0
+        elif block_y > 440:
+            block_y = 440
+        else:
+            block_y = block_y + direction_y * speed
+        #end if
+            
+        if circ_x == 0 +8:
+            hp_left = hp_left - 1
+        #end if
+            
+        if circ_x == block_x+8+8 and (circ_y<=block_y+40 and circ_y>=block_y):
+            Cdir_x = Cdir_x * -1
+            circ_x = circ_x + (Cdir_x*ball_speed)
+            
+        
+
+            
+        elif circ_x == 0 + 8 or circ_x==640 - 8: 
+            circ_x = 320            #//variables reset so that game restarts
+            circ_y = 240
+            direction_x = 0
+            direction_y = 0
+            Cdir_x = 4
+            Cdir_y = 4
+            
+            
+            
+            if hp_left == 0:
+                game_over = True    #//ends game after losing 3 times
+                screen.blit(text, textRect)
+                pygame.display.update()
+                print("Score of PLAYER 1:",score_1)
+                enter_name = input("Please enter your name for the scoreboard")
+            #end if  
+        else:
+            if circ_x != 0+8 and circ_x != 640-8:
+                circ_x = circ_x + Cdir_x
+            else:
+                Cdir_x = Cdir_x * -1
+                circ_x = circ_x + (Cdir_x*2)
+            #end if
+            if circ_y != 0 + 8 and circ_y != 480-8:
+                circ_y = circ_y + Cdir_y
+            else:
+                Cdir_y = Cdir_y * -1
+                circ_y = circ_y+ (Cdir_y*2)
+            #end if
+        #end if
+    else:
+        cpu_speed= int(input("Please enter the CPU speed you want (Higher speed, Higher difficulty)"))
         
     # -- Screen background is BLACK
     screen.fill (BLACK)
