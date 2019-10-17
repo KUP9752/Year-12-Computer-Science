397419bbb2a0e128f58e5df3a647bca8dfeb6008
KUP9752 <54801550+KUP9752@users.noreply.github.com>
2019-10-17 09:53:32 +0100

    Pong high score file io made

--- PyGame/PONG GAME/PONG .py
+++ PyGame/PONG GAME/PONG .py
@@ -1,11 +1,21 @@
 import pygame
 # -- Funtions
 def write_hiscore(name, score):
-    folder = open("HighScore.txt", "wt")
-    folder.write(name)
-    folder.write(score)
-    folder.close()
+    Rfolder = open("HighScore.txt", "rt")
+    Rscore = Rfolder.read()
+    Rlength = len(Rscore)
+    index = Rscore.find(":")
+    Rscore = Rscore[index+1:Rlength+1]
+   
+    if score>Rscore:
+        Wfolder = open("HighScore.txt", "wt")
+        Wfolder.write(name)
+        Wfolder.write(":")
+        Wfolder.write(score)
+        Wfolder.close()
+    #end if
 #end procedure
+        
 # -- Global Constants
 
 # -- Colours
