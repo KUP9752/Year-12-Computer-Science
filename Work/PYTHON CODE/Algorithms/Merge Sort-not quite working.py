28db22b9c4d7107a22558dfd95225103e3b54e2b
KUP9752 <54801550+KUP9752@users.noreply.github.com>
2020-05-04 12:24:16 +0100

    updated merge sort
    Still doesnt quite work

--- Work/PYTHON CODE/Algorithms/Merge Sort-not quite working.py
+++ Work/PYTHON CODE/Algorithms/Merge Sort-not quite working.py
@@ -1,7 +1,7 @@
-list1 = [4,12,18,5,3,1,10,11,2,14]
-left =[]
-right =[]
-def mergesort(list1):
+
+
+def notworkingmergesort(list1): #I think there was a mutaility problem of the lists in python for this recursion to work
+    
     if len(list1)> 1:
         mid = len(list1) //2
         for i in range(0,mid):
@@ -35,5 +35,57 @@ def mergesort(list1):
             rp+=1
             p+=1
         print("Completed pass: ", list1)
-mergesort(list1)
-print(list1)
+
+        
+list1 = [4,12,18,5,3,1,10,11,2,14]
+
+#I kept the merging and the sorting seperate
+def create_slist():
+    slist = []
+    for i in range(len(list1)):
+        slist.append(0)
+                      
+def merge(left,right):
+    if len(left) == 0:
+        return left
+    if len(right) == 0:
+        return right
+
+    lp = 0 #left pointer
+    rp = 0 #right pointer
+    p = 0 #pointer that sorts the original list
+
+    while lp< len(left) and rp < len(right):
+        if left[lp]<=right[rp]:
+            slist[p] = left[lp]
+        else:
+            slist[p] = right[rp]
+
+    while lp<len(left):
+        slist[p] = left[lp]
+        lp += 1
+        p +=1
+    while rp < len(right):
+        slist[p] = right[rp]
+        rp+=1
+        p+=1
+    return slist
+
+#I got some help to get past the mutability problem I had
+def mergesort(list1):
+    if len(list1)<1:
+        return list1
+
+    mid = len(list1)//2
+
+    return merge(left=mergesort(list1[:mid]),right=mergesort(list1[mid:]))
+
+create_slist()
+print(mergesort(list1))
+
+#maximum recursion reached no matter the length of my list, I dont know waht else to do
+
+
+    
+
+    
